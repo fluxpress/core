b5168e6f77dcb970f806a5e50838495c6b71f581
liangpengyv <me@laoliang.ink>
2024-08-24 01:37:41 +0800

    build: 更新 tsconfig.json

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 import { readFileSync } from 'node:fs'
 
 const pkg = JSON.parse(
-  readFileSync(new URL('../package.json', import.meta.url)),
+  readFileSync(new URL('../package.json', import.meta.url), 'utf-8'),
 )
 
 console.log(`Current @fluxpress/core version: ${pkg.version}`)
