8053b29b3515b1bd74ff87066c165ed22263f508
liangpengyv <me@laoliang.ink>
2024-09-03 23:46:38 +0800

    build: eslint 添加 typescript 支持

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,17 @@
+import tsEslint from 'typescript-eslint'
 import globals from 'globals'
 import js from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default [
-  { languageOptions: { globals: globals.node } },
+export default tsEslint.config(
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
   js.configs.recommended,
+  ...tsEslint.configs.recommended,
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
-]
+)
