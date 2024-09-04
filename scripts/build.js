80584c1e7091092ae2357dcfb2160248421d69a5
liangpengyv <me@laoliang.ink>
2024-09-04 18:11:59 +0800

    build: 优化构建方式

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,11 @@
 process.env.NODE_ENV = 'production'
 
 import { remove } from 'fs-extra'
+
 import { runCommand } from './utils.js'
 
+await remove('out')
+await runCommand({ command: 'tsc', name: 'tsc' })
+
 await remove('dist')
-await runCommand('tsc')
-await runCommand('rollup', ['--config'])
+await runCommand({ command: 'rollup --config', name: 'rollup' })
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,27 @@
-import { execa } from 'execa'
+import concurrently from 'concurrently'
 
-export async function runCommand(command, args) {
-  const runCommand = execa(command, args, { stdio: 'inherit' })
-  await runCommand
+/**
+ *
+ * @param {import('concurrently').ConcurrentlyCommandInput} command
+ * @param {Partial<import('concurrently').ConcurrentlyOptions>} options
+ * @returns
+ */
+export async function runCommand(command, options = {}) {
+  return new Promise((resolve) => {
+    const { result } = concurrently([command], options)
+    result.then(resolve)
+  })
+}
+
+/**
+ *
+ * @param {import('concurrently').ConcurrentlyCommandInput[]} commands
+ * @param {Partial<import('concurrently').ConcurrentlyOptions>} options
+ * @returns
+ */
+export async function runCommandsConcurrently(commands, options = {}) {
+  return new Promise((resolve) => {
+    const { result } = concurrently(commands, options)
+    result.then(resolve)
+  })
 }
