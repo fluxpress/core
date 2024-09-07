7924ececb141afe8b07db8ca3379fa2f59acfe3e
liangpengyv <me@laoliang.ink>
2024-09-07 15:27:06 +0800

    feat: 为工具类完善类型

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,11 +1,11 @@
 import path from 'node:path'
 import fs from 'fs-extra'
 
-export async function saveDataAsFile(filePath: string, data) {
+export async function saveDataAsFile<T>(filePath: string, data: T) {
   await fs.ensureDir(path.dirname(filePath))
   await fs.writeJson(filePath, data, { encoding: 'utf-8' })
 }
 
-export async function loadDataFromFile(filePath: string) {
+export async function loadDataFromFile<T>(filePath: string): Promise<T> {
   return await fs.readJson(filePath, { encoding: 'utf-8' })
 }
