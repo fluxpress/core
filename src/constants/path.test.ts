749c587d47356be6cbcc811d66f2855face36a31
liangpengyv <me@laoliang.ink>
2024-09-15 21:02:07 +0800

    fix: APP_ROOT_PATH

--- src/constants/path.test.ts
+++ src/constants/path.test.ts
@@ -2,6 +2,6 @@ import test from 'ava'
 
 import { APP_ROOT_PATH } from './path.js'
 
-test('test common demo', (t) => {
+test('APP_ROOT_PATH', (t) => {
   t.is(APP_ROOT_PATH, process.cwd())
 })
--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 import { findUp } from 'find-up-simple'
 
 // app root
-export const APP_ROOT_PATH = path.dirname(await findUp('package.json'))
+export const APP_ROOT_PATH = path.dirname((await findUp('package.json')) ?? '')
 
 // output
 export const OUTPUT_PATH = path.join(APP_ROOT_PATH, 'public')
